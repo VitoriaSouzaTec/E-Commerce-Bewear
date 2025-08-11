4b23d18e1bf327d14cfd9b37bffe54ceab65d0e5
Vitoria Souza <vitoriasouzatec@gmail.com>
2025-08-11 09:21:25 -0300

    feat: add products list and category products

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -1,13 +1,13 @@
-
+import { Header } from "@/components/common/header";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 import SignInForm from "./components/sign-in-form";
 import SignUpForm from "./components/sign-up-form";
 
-const Authentication = () => {
+const Authentication = async () => {
   return (
     <>
-      
+      <Header />
 
       <div className="flex w-full flex-col gap-6 p-5">
         <Tabs defaultValue="sign-in">
