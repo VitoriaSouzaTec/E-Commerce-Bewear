0b19b2db3089e754bb1e4511a97c563e237a12c4
Vitoria Souza <vitoriasouzatec@gmail.com>
2025-09-16 19:52:36 -0300

    fix: corrige URL hardcodeaa

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 
+console.log("Variável de ambiente carregada:", process.env.NEXT_PUBLIC_APP_URL);
+
 export const authClient = createAuthClient({
   baseURL: `${process.env.NEXT_PUBLIC_APP_URL}api/auth`,
 });
